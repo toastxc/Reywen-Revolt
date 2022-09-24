b0d573c6d8d4f9ad29d2366232691c90b67db9c6
ToastXC <100072983+toastxc@users.noreply.github.com>
2022-09-24 20:30:21 +0800

    Update .example.rs

--- .example.rs
+++ .example.rs
@@ -1,6 +1,7 @@
-   if content == "?hello" {
-                        rev_send(data.token.clone(), channel.clone(), "world!".to_string()));
+if content == "?hello" {
+                        rev_send(data.token.clone(), channel.clone(), "world!".to_string());
 
-                    }else if content == "?ping" {
+}else if content == "?ping" {
                         rev_send(data.token.clone(), channel.clone(), "Pong!!".to_string());
-                    };        
+                    };
+}
