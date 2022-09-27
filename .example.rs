fee4e0298f50886ae729fb864275f12e29ba9300
ToastXC <100072983+toastxc@users.noreply.github.com>
2022-09-27 23:52:25 +0800

    Delete .example.rs

--- .example.rs
+++
@@ -1,7 +0,0 @@
-if content == "?hello" {
-                        rev_send(data.token.clone(), channel.clone(), "world!".to_string());
-
-}else if content == "?ping" {
-                        rev_send(data.token.clone(), channel.clone(), "Pong!!".to_string());
-                    };
-}
