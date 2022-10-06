2e30aa52a17adc91ac551a124d98de77001a6534
ToastXC <100072983+toastxc@users.noreply.github.com>
2022-10-06 17:04:12 +0800

    Added async for bridge + message process

--- src/main.rs
+++ src/main.rs
@@ -25,12 +25,19 @@ mod lreywen;
 use lreywen::*;
 
 // network
-use futures_util::{StreamExt};
-use tokio_tungstenite::{connect_async};
+use futures_util::StreamExt;
+use tokio_tungstenite::connect_async;
 
 
 use std::str::from_utf8;
+
 use tokio;
+
+const PING: &str = r#"{
+    "type": "Ping",
+    "data": 0
+}"#;
+
 
 #[tokio::main]
 async fn main()  {
@@ -75,28 +82,35 @@ async fn main()  {
 
     let url = format!("wss://ws.revolt.chat/?format=json&version=1&token={token}");
 
- 
-     websocket(url, data, br).await;
+    websocket(url, data, br).await;
 
 }
 
 // establishes websocket connection
 pub async fn websocket(url: String, authen: Auth, br: BrConf) {
 
+
      let (ws_stream, _response) = connect_async(url).await.expect("Failed to connect");
      println!("init: websocket");
 
+
+     // this cant be in a program, because moving it requires defining an invalid type
+     // and it cant be here because then the process is async do idk what to do pwp
+     //tokio::time::sleep(Duration::from_secs(30));
+     //ws_stream.send(tokio_tungstenite::tungstenite::Message::Text(PING.to_string()));
+
      let (mut _write, read) = ws_stream.split();
 
-    let read_future = read.for_each(|message| async {
-         
+     let read_future = read.for_each(|message| async {
+        
+        
         let data = message.unwrap().into_data();
-       
         let out = from_utf8(&data).unwrap().to_string();
 
        // moved websocket main to self contained function for ease of use 
        newmain(authen.clone(), out, br.clone()).await;
-    });
+
+     });
 
     read_future.await;
 }
@@ -107,38 +121,31 @@ pub async fn websocket(url: String, authen: Auth, br: BrConf) {
 // bridge and message processing
 pub async fn newmain(authen: Auth, out: String, br: BrConf) {
 
-    let inval_message = rev_message_in(out.clone());
-    let inval2 = rev_message_in(out.clone());
+    let raw_message = rev_message_in(out);
 
-    match inval_message {
+    let (message, message2) = match raw_message {
         Err(_) => return,
-        Ok(_) => print!("")
-
-    };
-    
-    let message = inval_message;
-    
-    message_process(authen.clone(), message.expect("failed to process message")).await;
-    
-    // br does not require 'cleaned' messages
-    if br.enabled == true {
-        br_main(authen.clone(), inval2.unwrap(), br).await;
+        Ok(_) => (raw_message.as_ref().expect("REASON").clone(), raw_message.unwrap())
     };
 
+    tokio::join!(
+        br_main(authen.clone(), message2, br),
+        message_process(authen.clone(), message),
+        );
 }
+
+
 
 // main message engine 
 pub async fn message_process(data: Auth, message_in: RMessage) {
  
     let content = message_in.content.clone();
-
     // validity test
     if content == None {
         return
     }else if message_in.author == data.bot_id {
         return
     };
-
     let message = rev_message_clean(message_in).await;
 
     let content_vec =  content.as_ref().expect("failed to split vec").split(' ').collect::<Vec<&str>>();
@@ -148,7 +155,8 @@ pub async fn message_process(data: Auth, message_in: RMessage) {
     for x in 0..content_vec.len() -1 {
         content_min1 += &format!("{} ", content_vec[x + 1])
     };
- 
+
+  
     match &content_vec[0] as &str {
         
         "?Mog" | "?mog"  => send(data, message, ":01G7MT5B978E360NB6VWAS9SJ6:".to_string()).await,
