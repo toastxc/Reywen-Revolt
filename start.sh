8b8c2c95974d82ae032a6a7e3432409f9e1131ce
ToastXC <100072983+toastxc@users.noreply.github.com>
2022-10-19 19:39:46 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
 git clone https://github.com/toastxc/Reywen-Revolt.git
 cd Reywen-Revolt
 cargo build
-vim auth.json
-vim bridge.json
-cp target/debug/reywen2 .
-rm archive  Cargo.lock  Cargo.toml src target LICENSE  README.md target start.sh -rf
 echo "done"
