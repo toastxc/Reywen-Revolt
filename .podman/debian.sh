dd2b6a979d9328f7fcb279c30111615a93ad7da6
ToastXC <100072983+toastxc@users.noreply.github.com>
2022-12-06 11:03:57 +0800

    Changed to a run and build script

--- .podman/debian.sh
+++ .podman/debian.sh
@@ -1,8 +1,17 @@
-apt update -y
-apt upgrade -y
-apt install git openssl
-git clone https://github.com/toastxc/Reywen-Revolt.git
-cd Reywen-Revolt
-cargo build --release
-cd /
-cp Reywen-Revolt/config . -r
+reywen=$(echo ls | grep Reywen-Revolt)
+
+if [[ $reywen = "" ]]; then
+
+        apt update -y
+        apt upgrade -y
+        apt install git openssl
+        git clone https://github.com/toastxc/Reywen-Revolt.git
+        cd Reywen-Revolt
+        cargo build --release
+        cd /
+        cp Reywen-Revolt/config . -r
+else
+        ./Reywen-Revolt/target/release/reywen2
+
+
+fi
