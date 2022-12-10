f1c51250e07577be6d28e1a85f75c8de30c1402a
ToastXC <100072983+toastxc@users.noreply.github.com>
2022-12-10 17:49:21 +0800

    removed garbage from message.rs

--- src/plugins/message.rs
+++ src/plugins/message.rs
@@ -1,17 +1,14 @@
-use crate::{
-    Auth, RMessage, rev_message_clean, send, sendas};
+use crate::{Auth, RMessage, send, sendas};
 
 use serde::{Serialize, Deserialize};
 
 use crate::fs_str;
-
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MessageConf {
 
     pub enabled: bool
 }
-
 
 // main message engine 
 pub async fn message_process(details: Auth, message_in: RMessage) {
@@ -33,7 +30,7 @@ pub async fn message_process(details: Auth, message_in: RMessage) {
     }else if message_in.author == details.bot_id {
         return
     };
-    let message = rev_message_clean(message_in).await;
+    let message = message_in;
 
     let content_vec =  content.as_ref().expect("failed to split vec").split(' ').collect::<Vec<&str>>();
 
@@ -43,17 +40,14 @@ pub async fn message_process(details: Auth, message_in: RMessage) {
         content_min1 += &format!("{} ", content_vec[x + 1])
     };
 
-
     match &content_vec[0] as &str {
 
         "?Mog" | "?mog"  => send(details, message, ":01G7MT5B978E360NB6VWAS9SJ6:".to_string()).await,
         "?ver" | "?version" => send(details, message, "**Version**\nReywen: `2`\nRevX: `2`".to_string()).await,
-        "?echo" => send(details, message, content_min1).await,
+        "?echo" =>  send(details, message, content_min1).await,
         "?sendas" => sendas(details, message, content_vec).await,
-        "?whydoyounothaveanyfeatures:trol:" => send(details, message, "Reywen1 was an experiment to see if <@01FSRTTGJC1XJ6ZEQJMSX8Q96C> could make a bot for revolt with no libraries in rust\n Reywen2 was a test to see how much better they could make it with more experience in rust... if you want to make add features Reywen is not difficult to develop for \n".to_string()).await,
         _ => return
     };
 
 }
-
 
