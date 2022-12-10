8f838f714214519ee9b6e2045ab156685e152f70
ToastXC <100072983+toastxc@users.noreply.github.com>
2022-12-10 19:20:01 +0800

    fixed up plural with good practises

--- src/plugins/plural.rs
+++ src/plugins/plural.rs
@@ -1,14 +1,10 @@
-// dependancies 
-
 use crate::{Auth, RMessage, lib::message::*};
 use crate::fs_str;
 use serde::{Serialize, Deserialize};
 use crate::send;
 use mongodb::{options::ClientOptions, bson::doc};
-//use crate::rev_convert_reply;
 use crate::rev_send;
 use bson::Document;
-//use mongodb::results::CollectionType::Collection;
 use crate::rev_del;
 
 // config struct
@@ -26,44 +22,37 @@ struct Plural {
 
 }
 
-
-
-
 // plugin main is responsible for getting details and activiating functions based on conditions
-pub async fn plural_main(a: Auth, m: RMessage) {
+pub async fn plural_main(auth: Auth, message: RMessage) {
 
-    
     let conf = fs_str("config/plural.json")
         .expect("failed to read config/plural.json\n{e}");
 
-    let c: Plural = serde_json::from_str(&conf)
+    let plural: Plural = serde_json::from_str(&conf)
         .expect("Failed to deser plural.json");
-
-        
 
 
     // if the config channel matches the channel of the message received AND 
     // if the plugin is enabled, send ID
-    if c.enable == false {
+    if plural.enable == false {
         return
     
-    }else if c.channel_only == true && c.channel != m.channel {
+    }else if plural.channel_only == true && plural.channel != message.channel {
         return
     }; 
 
-    let content_raw = match m.content {
-        Some(_) => m.content.as_ref().unwrap(),
+    let content_raw = match message.content {
+        Some(ref a) => a,
         None => return,
     };
 
-    
     let content: Vec<&str> =  content_raw.split(' ').collect::<Vec<&str>>();
 
     if content[0] != "?p" {
         return
     
     }else if content.len() < 3 {
-        send(a, m, "**Reywen Masq**
+        send(auth, message, "**Reywen Masq**
              `search <name>`: Search for an entry in ReywenDB
              `insert <name> <avatar-url> <color>`: create a new entry
              `query <name>`: searches for entry and provides details
@@ -71,20 +60,18 @@ pub async fn plural_main(a: Auth, m: RMessage) {
              `rm <name>`: removes entry".to_string()).await;
         return
     
-    }else if m.author == a.bot_id {
+    }else if message.author == auth.bot_id {
         return
     };
 
     match &content[1] as &str {
 
-        "insert" => pl_insert(a, m.clone(), c, content).await,
-        "send" => pl_send(a, m.clone(), content, c).await,
-        "search" => cli_search(a, m.clone(), content[2], c).await,
-        "rm" => pl_remove(a, m.clone(), content[2], c).await,
-        "query" => cli_query(a, m.clone(), content[2], c).await,
-        //"generic" => pl_generic(a, m.clone(), content, c).await,
+        "insert" => pl_insert(auth, message.clone(), plural, content).await,
+        "send" => pl_send(auth, message.clone(), content, plural).await,
+        "search" => cli_search(auth, message.clone(), content[2], plural).await,
+        "rm" => pl_remove(auth, message.clone(), content[2], plural).await,
+        "query" => cli_query(auth, message.clone(), content[2], plural).await,
         _ => {},
-
     };
 
 }
@@ -94,102 +81,76 @@ async fn cli_query(auth: Auth, message: RMessage, content: &str, plural: Plural)
 
     match search {
         Some(a) => {
-
             let masq_data = format!("```json\n\"name:\" \"{}\"\n \"avatar\" \"{}\"\n\"colour:\" \"{}\"", 
                                     a.name.unwrap(), a.avatar.unwrap(), a.colour.unwrap());
             send(auth, message, masq_data).await;
         },
-
         None  => send(auth, message, "**Object not found**".to_string()).await,
     };
-
-
-
 }
 
-async fn cli_search(a: Auth, m: RMessage, i: &str, c: Plural)  {
+async fn cli_search(auth: Auth, message: RMessage, content: &str, plural: Plural)  {
 
-    let res = pl_search(i, c).await;
+    let res = pl_search(content, plural).await;
     
     let strr = match res {
         Some(_) => "**Object found**",
         None => "**Object not found**",
     };
 
-    send(a, m, strr.to_string()).await
-
-
+    send(auth, message, strr.to_string()).await
 }
+async fn pl_remove(auth: Auth, message: RMessage, content: &str, plural: Plural)  {
 
-
-async fn pl_remove(a: Auth, m: RMessage, i: &str, c: Plural)  {
-
-
-    
-    let name = i;
-    println!("{name}");
     let param = format!("mongodb://{}:{}@{}:{}",
-                        c.db_usrname, c.db_pswd, c.db_ip, c.db_port);
+                        plural.db_usrname, plural.db_pswd, plural.db_ip, plural.db_port);
 
-    let client_options = ClientOptions::parse(param).await.unwrap();
-
-    let client = mongodb::Client::with_options(client_options);
+    let client = mongodb::Client::with_options(ClientOptions::parse(param).await.unwrap());
 
     let client = match client {
         Ok(_) => client.unwrap(),
         Err(e) => {println!("failed:\n{e}"); return},
     };
 
-    let db = client.database("test");
+    let masks = client.database("test").collection::<Masquerade>("profiles");
 
-    let masks = db.collection::<Masquerade>("profiles");
-
-    let userquery = masks.find_one(doc! { "name": name }, None).await;
-
+    let userquery = masks.find_one(doc! { "name": content }, None).await;
 
     if userquery.is_ok() != true {
-        send(a, m, "**Failed to get details**".to_string()).await;
+        send(auth, message, "**Failed to get details**".to_string()).await;
         println!("WARN: pl_remove failed to connect");
     
     }else if userquery.unwrap().is_some() != true {
-        send(a, m, "**No object found**".to_string()).await
+        send(auth, message, "**No object found**".to_string()).await
     }else {
-        let del_res = masks.delete_one(doc!{"name": name}, None ).await;
-        send(a.clone(), m.clone(), "**Object found, deleting...**".to_string()).await;
+        let del_res = masks.delete_one(doc!{"name": content}, None ).await;
+        send(auth.clone(), message.clone(), "**Object found, deleting...**".to_string()).await;
 
         match del_res {
-            Ok(_) => send(a, m, "**Successfully deleted**".to_string()).await,
-            Err(e) => send(a, m, format!("**Error**\n```text\n{e}")).await,
+            Ok(_) => send(auth, message, "**Successfully deleted**".to_string()).await,
+            Err(e) => send(auth, message, format!("**Error**\n```text\n{e}")).await,
         };
     };
-
 }
 
+async fn pl_search(content: &str, plural: Plural) -> Option<Masquerade> {
 
-
-
-async fn pl_search(i: &str, c: Plural) -> Option<Masquerade> {
-
-
-    let name = i;
-    println!("{name}");
     let param = format!("mongodb://{}:{}@{}:{}",
-                        c.db_usrname, c.db_pswd, c.db_ip, c.db_port);
+                        plural.db_usrname, plural.db_pswd, plural.db_ip, plural.db_port);
 
-    let client_options = ClientOptions::parse(param).await.unwrap();
+    let client = mongodb::Client::with_options(ClientOptions::parse(param).await.unwrap());
 
-    let client = mongodb::Client::with_options(client_options);
-
+     
     let client = match client {
-        Ok(_) => client.unwrap(),
-        Err(e) => {println!("failed:\n{e}"); return None},
+        Ok(a) => a,
+        Err(e) => panic!("MONGODB_FAILED_SEARCH:\n{e}"),
     };
 
     let db = client.database("test");
 
     let masks = db.collection::<Masquerade>("profiles");
 
-    let userquery = masks.find_one(doc! { "name": name }, None).await;
+    let userquery = masks.find_one(doc! { "name": content }, None).await;
 
     match userquery {
         Ok(a) => return a,
@@ -197,7 +158,7 @@ async fn pl_search(i: &str, c: Plural) -> Option<Masquerade> {
     };
 }
 
-async fn pl_send(a: Auth, m: RMessage, i: Vec<&str>, c: Plural) {
+async fn pl_send(auth: Auth, message: RMessage, i: Vec<&str>, c: Plural) {
 
     let profile = pl_search(i[2], c).await;
 
@@ -216,64 +177,51 @@ async fn pl_send(a: Auth, m: RMessage, i: Vec<&str>, c: Plural) {
         masquerade: Some(profile.unwrap()),
     };
 
+    
     tokio::join!(
 
-        rev_send(a.clone(), m.clone(), payload),
-        rev_del(a.clone(), m.clone()),
-     );
+        rev_send(auth.clone(), message.clone(), payload),
+        rev_del(auth, message),
+        );
 
     }else {
-
-        send(a, m, "**Object  not found**".to_string()).await;
-
+        
+        send(auth, message, "**Object  not found**".to_string()).await;
     };
-
-
-
-
-
 }
 
-
-async fn pl_insert(a: Auth, m: RMessage, c: Plural, i: Vec<&str>){
+async fn pl_insert(auth: Auth, message: RMessage, plural: Plural, content: Vec<&str>){
 
      let param = format!("mongodb://{}:{}@{}:{}",
-                        c.db_usrname, c.db_pswd, c.db_ip, c.db_port);
+                        plural.db_usrname, plural.db_pswd, plural.db_ip, plural.db_port);
 
+     let client = mongodb::Client::with_options(ClientOptions::parse(param).await.unwrap());
 
-      let client_options = ClientOptions::parse(param).await.unwrap();
+         
+     let client = match client {
+        Ok(a) => a,
+        Err(e) => {println!("MONGODB_insert:\n{e}"); return},
+    
+     };
+      
+     let collection = client.database("test").collection::<Document>("profiles");
+      
+     let data: Vec<Document> =
+         vec![
 
-      let client = mongodb::Client::with_options(client_options);
+         doc! {
+         "name": content[2],
+         "avatar": content[3],
+         "colour": content[4],
+     }];
 
-      let client = match client {
-        Ok(_) => client.unwrap(),
-        Err(e) => {println!("failed:\n{e}"); return},
-    };
-
-      let db = client.database("test");
-
-      let collection = db.collection::<Document>("profiles");
-
-      let mut data = vec![];
-
-      let (name, avatar, colour) = (i[2], i[3], i[4]);
-      let masks = doc! {
-         "name": name,
-         "avatar": avatar,
-         "colour": colour,
-      };
-
-      data.push(masks);
-
-
-      let userquery = collection.insert_many(data, None).await;
+     let userquery = collection.insert_many(data, None).await;
 
       if userquery.is_ok() != true {
-          send(a, m, "**Failed to connect**".to_string()).await;
+          send(auth, message, "**Failed to connect**".to_string()).await;
           println!("WARN: pl_insert failed to insert");
 
       }else {
-          send(a, m, "**Object valid, inserting...**".to_string()).await;
+          send(auth, message, "**Object valid, inserting...**".to_string()).await;
       };
-
 }
