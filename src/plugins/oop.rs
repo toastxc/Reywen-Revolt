dad2ceb42c482e8788968bc405ba9ca951733f78
ToastXC <100072983+toastxc@users.noreply.github.com>
2022-12-27 11:50:29 +0800

    Added functional masq abstration

--- src/plugins/oop.rs
+++ src/plugins/oop.rs
@@ -1,7 +1,7 @@
   // a sandbox for experimenting with OOP design patterns for abstraction and DX
     // lets create a simple hello world thingo
 
-use crate::lib::{conf::Auth, message::{RMessage, Masquerade}};
+use crate::{lib::{conf::Auth, message::{RMessage, Masquerade, RMessagePayload}}, rev_x::rev_send};
 use crate::send;
 
 #[derive(Debug)]
@@ -22,8 +22,18 @@ impl ReywenSys {
             Self::Send{content: c } => {
                 send(&auth.token, &message_input, c ).await
             },
+            
+            
             Self::SendMasq { content, masq } => {
-                
+      
+                let payload = RMessagePayload {
+                    content: Some(content.to_string()),
+                    attachments: None,
+                    replies: None,
+                    masquerade: Some(masq.to_owned()),   
+                    
+                };
+                rev_send(&auth.token, &message_input.channel, payload).await          
             }
             Self::Delete => {},
             
@@ -40,15 +50,20 @@ impl ReywenSys {
 
     let content = input_message.content.clone().unwrap();
     
-
+    let masq: Masquerade  = Masquerade {
+        avatar: Some("https://autumn.revolt.chat/avatars/tpDMc0zLiHzg9ZBLCHwF-7a50PRWwy9dsUMZaGi_2m".to_string()),
+        name: Some("Reywen-MASQ".to_string()),
+        colour: None,            
+    };
+    
      let message = match &content as &str {
-       "greer" => ReywenSys::Send{ content: ("greer")},
-       //"" => ReywenSys::Send{ content: ("balls")},
+       "test hello" => ReywenSys::Send{ content: ("World")},
+       "test masq" => ReywenSys::SendMasq { content: ("hewo"), masq: (masq)},
        __ => ReywenSys::None,
     };    
     
     
     message.run(auth, input_message.clone()).await;
-    println!("{:?}", message);
+  
 }
 
