ba6466d1f4d8fe21959a35f5eb165f577bdecb8a
ToastXC <100072983+toastxc@users.noreply.github.com>
2023-01-03 19:07:38 +0800

    Update plugin.rs

--- example/plugin.rs
+++ example/plugin.rs
@@ -1,43 +1,65 @@
-// dependancies 
 
-use crate::{Auth, RMessage, lib::message::*};
-use crate::fs_str;
-use serde::{Serialize, Deserialize};
+// serde is the toolchain for interpreting data from jsons
+use serde::{Deserialize, Serialize};
+// these are all internal libraries
+use crate::{fs::fs_str, lib::{conf::Auth, message::{RMessage, RMessagePayload}}};
+use super::oop::Reywen;
 
-
-// config struct
-// this optional struct adds configurable paramaters that are hot changeable, config files are
-// jsons and usually stored in config/
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct Mogus {
-    pub enable: bool,
-    pub id: String,
-    pub channel: String
+// the configuration for a basic plugin, optional_channel does not have to exist but enabled does.
+#[derive(Debug, Deserialize, Serialize)]
+struct MogConf {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    optional_channel: Option<String>,
+    enabled: bool,
 }
 
-
-// plugin main is responsible for getting details and activiating functions based on conditions
-pub async fn plugin_main(auth: Auth, input_message: RMessage) {
-
-    let conf = fs_str("config/plugin.json")
-        .expect("failed to read config/plugin.json\n{e}");
-
-    let c: Mogus = serde_json::from_str(&conf.unwrap())
-        .expect("Failed to deser plugin.json");
-
-
-
-    // if the config channel matches the channel of the message received AND 
-    // if the plugin is enabled, send ID
-    if c.enabled == true && c.channel == input_message.channel {
-
-        send(id).await
-
+// main function for plugin
+// this will activate when a message is received from websocket
+pub async fn e6_main(auth: Auth, input_message: RMessage) {
+    
+     // imports the json from config/mogus.json
+     let conf: String = fs_str("config/mogus.json").expect("failed to read config mogus.json\n{e}");
+     let mogus_conf: MogConf = serde_json::from_str(&conf).expect("Failed to deser mogus.json{e}");
+     
+     // if mogus is disabled, close function
+     if !mogus_conf.enabled {
+         return
+     
+     // if websocket message is empty, close function    
+     }else if input_message.content.is_none() {
+         return
+     };
+     // creates a vector of messages from input message
+     let temp = input_message.content.unwrap();
+     let convec: Vec<&str> = temp.split(' ').collect();
+     
+    
+    // now, to create a new client
+    let client = Reywen::new(auth);
+    
+    // the Revolt api offers many different options for message payloads, this is a simple one
+    let payload = RMessagePayload::new()
+        .content("I love amogus");
+    
+    // if the first message someone sends is 'amogus', send ^ payload
+    if convec[0] == "?amogus" {
+        client.clone().send(payload, &input_message.channel).await;
     };
-
-
-    // there are many other possible features of reywen plugins but this is a good simple starting
-    // point
+   
+    
+    // same example with a match
+    let word: &'static str = match convec[0] {
+        "?amogi" => "amongus",
+        "?mogus" => "mogmogmog",
+        _ => "", 
+        
+    }; 
+    
+    let newpayload = RMessagePayload::new().content(&word);
+       
+    client.send(newpayload, &input_message.channel).await;
+    
+    // this is a very new and experimental way of using reywen... and as you can see its not perfect yet
+    // more methods will be derived and ported over, as well as more abstration
 
 }
-
