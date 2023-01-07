4e8873a800a0361ba01a86e5f0d67e6d093c51a9
ToastXC <100072983+toastxc@users.noreply.github.com>
2023-01-07 17:50:53 +0800

    derived new methods for oop

--- src/plugins/oop.rs
+++ src/plugins/oop.rs
@@ -1,41 +1,61 @@
 
 
-use crate::{lib::{conf::Auth, message::{RMessage, Masquerade, RMessagePayload, RReplies}}, rev_x::{rev_send, rev_del_2}};
+use crate::{lib::{conf::Auth, message::{RMessage, Masquerade, RMessagePayload, RReplies}}, rev_x::{rev_send, rev_del_2, rev_kick, rev_fetch_channel}, plugins::lreywen::send};
 use crate::plugins::lreywen::reply_from;
 #[derive(Debug, Clone, Default)]
 pub struct Reywen {
-  auth: Auth
+    auth: Auth,
+    input_message: RMessage,
+    convec: Vec<&'static str>
+
 }
 
 impl Reywen {
     
-    pub fn new(auth: Auth) -> Self {
+    pub fn new(auth: Auth, input_message: &RMessage) -> Self {
+        let input_message = input_message.to_owned();
+        let convec: Vec<&str> = input_message.content.unwrap().split(' ').collect();
+
         Reywen
         {
-            auth
+            auth, input_message, convec
         }
     }
     
   
-    pub async fn send(self, payload: RMessagePayload, channel: &str) -> Self {
+    pub async fn send(self, payload: RMessagePayload) -> Self {
     
         
-        rev_send(&self.auth.token, channel, payload).await;
+        rev_send(&self.auth.token, &self.input_message.channel, payload).await;
         self
     }
-    pub async fn sender(self, content: &str, input_message: &RMessage) -> Self {
+    pub async fn sender(self, content: &str) -> Self {
         
+
         let payload = RMessagePayload {
             content: Some(String::from(content)),
             attachments: None,
-            replies: Some(vec![reply_from(input_message)]),
+            replies: Some(vec![reply_from(&self.input_message)]),
             masquerade: None,
         };
-        rev_send(&self.auth.token, &input_message.channel, payload).await;
+        rev_send(&self.auth.token, &self.input_message.channel, payload).await;
         self
     }
-    pub async fn delete_msg(self, message_id: &str, input_message: &RMessage, ) -> Self {
-        rev_del_2(&self.auth.token, message_id, &input_message.channel).await;
+    pub async fn delete_msg(self, message_id: &str) -> Self {
+        rev_del_2(&self.auth.token, message_id, &self.input_message.channel).await;
+        self
+    }
+
+    pub async fn member_kick(self, user: &str) -> Self {
+
+        let server = rev_fetch_channel(&self.input_message.channel, &self.auth.token).await.unwrap().server;
+        let payload = format!("**Kicking {}**", user);
+        let user = user.replace(['@', '<', '>'], "");
+
+        tokio::join!(
+                rev_kick(&self.auth.token, &user, &server),
+                send(&self.auth.token, &self.input_message, &payload),
+                );
         self
     }
 }
@@ -71,100 +91,27 @@ impl RMessagePayload {
 
 
 pub async fn oop_main(auth: Auth, input_message: &RMessage) {
-    // stops function from running
-    return;
+
+
+    let client = Reywen::new(auth, input_message);
+
+
+
     
-    // new client based on stored auth keys
-    let client = Reywen::new(auth);
-  
-    // clients can be constructed from hard coded values like so (commented for warning)
-    //let other_client = Reywen::new(Auth {token: String::from("TOKEN"), bot_id: String::from("ID"), sudoers: Vec::new()});
-  
-    // Masquerade is a feature in revolt for sending messages with a 
-    // different profile, requires permissions from a server
-    let masq = Masquerade { name: Some(String::from("Greg")), avatar: None, colour: None };
-    
-  
-   // replies payload
-   let replies = vec![
-       RReplies {
-           id: input_message._id.to_owned(),
-           mention: false,
-       }  
-   ];
-    
-    
-    // RMessagePayload is the type for sending messages to Revolt API
-    // all fields are optional
-    let payload  = RMessagePayload::new()
-        // message content
-        .content("Hello i am greg!")
-        .masquerade(masq)
-        // reply from constructs a reply payload based on an input message
-        .reply(input_message)
-        // replies allows manual control values for replies
-        .replies(replies);
-        
-   
-    //this is a simple example of message logic
-    // if the content of the previous message from websocket == "?tester"
-    // send payload as defined ^
-    if input_message.content == Some(String::from("?tester")) {
-        client.clone().send(payload, &input_message.channel).await;
-    };
-    
-    // another method, sender is even simpler but grants less control over options
-    client.clone().sender("owo", input_message).await;
-    
-    
-    // usually bots have many commands and operations, and for this
-    // a match is much more practical
-    // another thing we'll do  is split the input message into a vector
-    // seperated by spaces, unfortunately handling vectors the wrong way
-    // can lead to crashes so we will handle that too
-    
-    // this closes the function if there is no content
-    let input = match input_message.clone().content {
-        None => return,
-        Some(a) => a,
-    };
-    
-    // cconvec short for content vector (self explanatory)
-    let convec: Vec<&str> = input.split(' ').collect();
-    if convec.is_empty() {return};
-    
-    // matches make code a lot cleaner for multiple arguements
-    let sender_content = match convec[0] {
-        
-        // if input == ?mog, define sender as AMOGUS
-        "?mog" => "AMOGUS",
-        "i dont like reywen" => "meanie ;w;",
-        
-        // if no conditions are met, return an empty string
-        _ => return
-    };
-    
-    client.sender(sender_content, input_message).await;
-    
-    
-    // the message input is a bad word, remove it
-    if convec.contains(&"badword") {
-        client.delete_msg(&input_message._id, input_message).await;  
-    };
-    
-    // a more robust solution for banned words
-    let wordlist = vec!["?word1", "?word1", "?word1", "?word1", "?word1"];
-    
-    for x in wordlist.iter() {
-        if convec.contains(x) {
-            client.delete_msg(&input_message._id, input_message).await;  
-        }
-    };
-    
-    
-    
-    
-     
-    
-    
+    if client.convec.contains(&"sus") {
+        client.clone().member_kick(&client.input_message.author).await;
+    }
+
+
+    client.
+
+
+
+
 }
+
+
+
+
+
+
