18f86ea7b7cfb7600daf3a86ff74278cfc53cf46
ToastXC <100072983+toastxc@users.noreply.github.com>
2023-01-07 21:09:39 +0800

    new example in oop.rs for methods

--- src/plugins/oop.rs
+++ src/plugins/oop.rs
@@ -6,19 +6,22 @@ use crate::plugins::lreywen::reply_from;
 pub struct Reywen {
     auth: Auth,
     input_message: RMessage,
-    convec: Vec<&'static str>
+
 
 }
 
 impl Reywen {
     
     pub fn new(auth: Auth, input_message: &RMessage) -> Self {
+
+
+
         let input_message = input_message.to_owned();
-        let convec: Vec<&str> = input_message.content.unwrap().split(' ').collect();
+
 
         Reywen
         {
-            auth, input_message, convec
+            auth, input_message
         }
     }
     
@@ -93,25 +96,90 @@ impl RMessagePayload {
 pub async fn oop_main(auth: Auth, input_message: &RMessage) {
 
 
+    // reywen is the main method for this bot,
+    // it can be used to interact with the API both normally and with abstrated methods
     let client = Reywen::new(auth, input_message);
 
+    // crash condition checks for a list of undesireable
+    // the second variable is an optional 'command prefix',
+    if crash_condition(input_message, Some("?m")) {return};
 
 
-    
-    if client.convec.contains(&"sus") {
-        client.clone().member_kick(&client.input_message.author).await;
-    }
+    // vector of content
+    let convec = convec(input_message);
 
 
-    client.
+    // matches all possilbe inputs and defines desired results as an str to be sent
+    let payload = match convec[1] as &str {
+        "?mog" => ":01G7MT5B978E360NB6VWAS9SJ6:",
+        "VeiledProduct" => "no",
+        "sussing" => "amogus",
+        _ => "NoneOption%",
+    };
+    if payload != "NoneOption%" {
+        client.clone().sender(payload).await;
+    };
+
+    // this is an example of send (not sender)
+    // its lower level, giving you more control over reywen while
+    // requiring more boilerplate.
+    // construct a payload and then send it!
+
+    let payload = RMessagePayload::new().content("world").reply(input_message);
+
+    if convec[1] == String::from("hello!") {
+
+        client.clone().send(payload).await;
+    };
+    // the available fields on RMessagePayload are the same as the API request requirements,
+    // with additional fields for abstration
 
 
 
+
+    // there are two main ways to use delete_msg, automated and command line
+    // both of which would most often be used for moderation
+    // there are alternate uses but they will not be covered here
+
+    // convec way
+    if convec.len() > 2 {
+        if convec[1] == "?del" {
+            client.clone().delete_msg(convec[2]).await;
+        };
+    };
+
+    // input_message way
+    // this example is a banned word list
+    let wordlist = vec!["badword", "anotherbadword!!", "illegalwords"];
+
+    for x in wordlist.iter() {
+        if convec.contains(x) {
+            client.clone().delete_msg(&input_message._id).await;
+
+        };
+    };
+
+    // kicking a user can simply be done by specifying the user ID
+    //client.member_kick("USERID")
+    // use this method with care, while i can't stop anyone i'd rather that reywen
+    // is not used for spamming or abuse on revolt
+
+    // simutanious actions
+    // for network applications such as reywen multithreading is not needed
+    // instead tasks can run at the same time on the same thread with tokio
+
+    if &convec[1] as &str == "?async" {
+        tokio::join!(
+
+                client.clone().sender("hey!"),
+                client.delete_msg(&input_message._id),
+                );
+    };
+    // notice the lack of await - its handled by tokio join
+
+
+    // there are many other methods and more comming soon! but this is enough of an example
+    // to get started, enjoy ^^ - Toast
 
 }
-
-
-
-
-
 
