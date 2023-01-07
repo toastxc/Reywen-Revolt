0fcbfedd910cda4787d5742001cf31c4d7ee728c
ToastXC <100072983+toastxc@users.noreply.github.com>
2023-01-07 17:50:20 +0800

    Added channel info for lib

--- src/lib/message.rs
+++ src/lib/message.rs
@@ -65,3 +65,54 @@ pub struct RReplies {
     pub id: String,
     pub mention: bool,
 }
+
+#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+pub struct RChannelFetch {
+    pub channel_type: String,
+    pub _id: String,
+    pub server: String,
+    pub name: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub description: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub icon: Option<RChannelIcon>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub default_permissions: Option<RPermissions>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub role_permissions: Option<RPermissionRoles>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub nsfw: Option<bool>,
+}
+#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+pub struct RChannelIcon {
+    pub _id: String,
+    pub tag: String,
+    pub filename: String,
+    pub metadata: FileType,
+    pub content_type: String,
+    pub size: u32,
+    pub deleted: bool,
+    pub reported: bool,
+    pub message_id: String,
+    pub user_id: String,
+    pub server_id: String,
+    pub object_id: String,
+
+}
+#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+pub struct RPermissions {
+    pub a: u32,
+    pub d: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+pub struct RPermissionRoles {
+    pub property1: RPermissions,
+    pub property2: RPermissions,
+}
+#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+pub struct FileType {
+    #[serde(rename = "type")]
+    pub typer: String,
+}
+
