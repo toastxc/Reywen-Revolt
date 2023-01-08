15f16d3181c4e9cad8e6f41d8c0d6b5724bc7e98
ToastXC <100072983+toastxc@users.noreply.github.com>
2023-01-08 13:36:20 +0800

    Created new example file

--- example/plugin.rs
+++ example/plugin.rs
@@ -1,65 +1,86 @@
+pub async fn example_main(auth: Auth, input_message: &RMessage) {
+    // reywen is the main method for this bot,
+    // it can be used to interact with the API both normally and with abstrated methods
+    let client = Reywen::new(auth, input_message);
 
-// serde is the toolchain for interpreting data from jsons
-use serde::{Deserialize, Serialize};
-// these are all internal libraries
-use crate::{fs::fs_str, lib::{conf::Auth, message::{RMessage, RMessagePayload}}};
-use super::oop::Reywen;
+// crash condition checks for a list of undesireable
+    // the second variable is an optional 'command prefix',
+    if crash_condition(input_message, Some("?m")) {return};
 
-// the configuration for a basic plugin, optional_channel does not have to exist but enabled does.
-#[derive(Debug, Deserialize, Serialize)]
-struct MogConf {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    optional_channel: Option<String>,
-    enabled: bool,
-}
 
-// main function for plugin
-// this will activate when a message is received from websocket
-pub async fn e6_main(auth: Auth, input_message: RMessage) {
-    
-     // imports the json from config/mogus.json
-     let conf: String = fs_str("config/mogus.json").expect("failed to read config mogus.json\n{e}");
-     let mogus_conf: MogConf = serde_json::from_str(&conf).expect("Failed to deser mogus.json{e}");
-     
-     // if mogus is disabled, close function
-     if !mogus_conf.enabled {
-         return
-     
-     // if websocket message is empty, close function    
-     }else if input_message.content.is_none() {
-         return
-     };
-     // creates a vector of messages from input message
-     let temp = input_message.content.unwrap();
-     let convec: Vec<&str> = temp.split(' ').collect();
-     
-    
-    // now, to create a new client
-    let client = Reywen::new(auth);
-    
-    // the Revolt api offers many different options for message payloads, this is a simple one
-    let payload = RMessagePayload::new()
-        .content("I love amogus");
-    
-    // if the first message someone sends is 'amogus', send ^ payload
-    if convec[0] == "?amogus" {
-        client.clone().send(payload, &input_message.channel).await;
+    // vector of content
+    let convec = convec(input_message);
+
+
+    // matches all possilbe inputs and defines desired results as an str to be sent
+    let payload = match convec[1] as &str {
+        "?mog" => ":01G7MT5B978E360NB6VWAS9SJ6:",
+        "VeiledProduct" => "no",
+        "sussing" => "amogus",
+        _ => "NoneOption%",
     };
-   
-    
-    // same example with a match
-    let word: &'static str = match convec[0] {
-        "?amogi" => "amongus",
-        "?mogus" => "mogmogmog",
-        _ => "", 
-        
-    }; 
-    
-    let newpayload = RMessagePayload::new().content(&word);
-       
-    client.send(newpayload, &input_message.channel).await;
-    
-    // this is a very new and experimental way of using reywen... and as you can see its not perfect yet
-    // more methods will be derived and ported over, as well as more abstration
+    if payload != "NoneOption%" {
+        client.clone().sender(payload).await;
+    };
+
+    // this is an example of send (not sender)
+    // its lower level, giving you more control over reywen while
+    // requiring more boilerplate.
+    // construct a payload and then send it!
+
+    let payload = RMessagePayload::new().content("world").reply_from(input_message);
+
+    if convec[1] == String::from("hello!") {
+
+        client.clone().send(payload).await;
+    };
+    // the available fields on RMessagePayload are the same as the API request requirements,
+    // with additional fields for abstration
+
+
+
+    // there are two main ways to use delete_msg, automated and command line
+    // both of which would most often be used for moderation
+    // there are alternate uses but they will not be covered here
+
+    // CLI
+    if convec.len() > 2 {
+        if convec[1] == "?del" {
+            client.clone().delete_msg(convec[2]).await;
+        };
+    };
+
+    // automated
+    // this example is a banned word list
+    let wordlist = vec!["badword", "anotherbadword!!", "illegalwords"];
+
+    for x in wordlist.iter() {
+        if convec.contains(x) {
+            client.clone().delete_msg(&input_message._id).await;
+
+        };
+    };
+
+    // kicking a user can simply be done by specifying the user ID
+    //client.member_kick("USERID")
+    // use this method with care, while i can't stop anyone i'd rather that reywen
+    // is not used for spamming or abuse on revolt
+
+    // simutanious actions
+    // for network applications such as reywen multithreading is not needed
+    // instead tasks can run at the same time on the same thread with tokio
+    if &convec[1] as &str == "?async" {
+        tokio::join!(
+                client.clone().sender("hey!"),
+                client.delete_msg(&input_message._id),
+                );
+    };
+    // notice the lack of await - the future is handled by tokio join
+    // this tool is restricted to revolt's API rate limit
+
+
+    // there are many other methods and more comming soon! but this is enough of an example
+    // to get started, enjoy ^^ - Toast
 
 }
+
