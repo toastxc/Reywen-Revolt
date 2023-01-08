c9a48c3431f8101b2907e2a48934735935861516
ToastXC <100072983+toastxc@users.noreply.github.com>
2023-01-08 15:17:05 +0800

    i cant make comments

--- src/lib/mongo.rs
+++ src/lib/mongo.rs
@@ -1,6 +1,5 @@
 // external
 use mongodb::{options::ClientOptions, bson::doc};
-use bson::Document;
 #[derive(Debug, Default)]
 pub struct RMongo {
     pub username: String,
@@ -10,7 +9,7 @@ pub struct RMongo {
     pub database: String
 }
 
-
+#[allow(dead_code)]
 pub async fn mongo_db(mut mongo: RMongo) -> mongodb::Database {
 
     if mongo.ip.is_none() {
@@ -31,41 +30,58 @@ pub async fn mongo_db(mut mongo: RMongo) -> mongodb::Database {
 }
 
 impl RMongo {
+    #[allow(dead_code)]
     pub fn new() -> Self {
         Self::default()
     }
+    #[allow(dead_code)]
     pub fn username(mut self, username: &str) -> Self {
         self.username = String::from(username);
         self
     }
+    #[allow(dead_code)]
     pub fn password(mut self, password: &str) -> Self {
         self.password = String::from(password);
         self
     }
+    #[allow(dead_code)]
     pub fn ip(mut self, ip: &str) -> Self {
         self.ip = Some(String::from(ip));
         self
     }
+    #[allow(dead_code)]
     pub fn port(mut self, port: &str) -> Self {
-        self.ip = Some(String::from(port));
+        self.port = Some(String::from(port));
         self
     }
+    #[allow(dead_code)]
     pub fn database(mut self, db: &str) -> Self {
-        self.password = String::from(db);
+        self.database = String::from(db);
         self
     }
 
 }
 
-async fn womp() {
+// example usage of mongodb
+#[allow(dead_code)]
+async fn example_usage() {
 
-    let womp = RMongo::new()
-        .username("helo")
-        .password("i forgor")
-        .database("the mongo one");
+    // define your credentials
+    let dbinfo = RMongo::new()
+        .username("github")
+        .password("hello")
+        .database("test");
 
+    // establish connection with mongodb
+    let db = mongo_db(dbinfo).await;
 
-    let database = mongo_db(womp).await;
+    // from here, mongodb can be used normally
+    let collection = db.collection::<String>("names");
+
+    let _ = collection.find_one(doc!("name": "jeff"), None).await;
 
 }
+
+
+
 
