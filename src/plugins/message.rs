a11d0fda29c2d451b627513c6da5d96f823bcc90
ToastXC <100072983+toastxc@users.noreply.github.com>
2023-01-08 19:25:57 +0800

    documented message.rs

--- src/plugins/message.rs
+++ src/plugins/message.rs
@@ -13,17 +13,22 @@ pub struct MessageConf {
 pub async fn message_main(auth: Auth, input_message: &RMessage) {
 
     // import config
-    let conf = fs_str("config/message.json").expect("failed to read config/message.json\n{e}");
+    let conf = fs_str("config/message.json")
+        .expect("failed to read config/message.json\n{e}");
 
     let message_conf: MessageConf = serde_json::from_str(&conf)
             .expect("Failed to deser message.json");
 
+    // return if this plugin is disabled
     if !message_conf.enabled {return};
 
+    // covers vector crash conditions
     crash_condition(input_message, None);
 
+    // content vector
    let convec = convec(input_message);
 
+    // created session based on credentials
    let client = Reywen::new(auth, input_message);
 
    let mes = match convec[0] as &str {
@@ -32,6 +37,7 @@ pub async fn message_main(auth: Auth, input_message: &RMessage) {
         "?ver" | "?version" => "Reywen is rolling release, there is no release numbers only commits :trol:",
         _ => ""
     };
+   // if applicable, send
    if mes != "" {
        client.sender(mes).await;
    };
