c4dc68a647e25270c8376684dee2f2505ae0fab3
ToastXC <100072983+toastxc@users.noreply.github.com>
2023-01-13 16:23:59 +0800

    rewrote main with new methods

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,24 @@ mod plugins {
     pub mod message;
     pub mod plural;
     pub mod shell;
-
 }
 use crate::plugins::{
-    bridge::br_main,
-    e6::e6_main,
-    message::message_main,
-    plural::plural_main,
-    shell::shell_main,
+    bridge::br_main, e6::e6_main, message::message_main, plural::plural_main, shell::shell_main,
 };
-
 
 // libraries
 mod lib {
     pub mod fs;
     pub mod lreywen;
+    pub mod mongo;
     pub mod oop;
     pub mod rev_x;
-    pub mod mongo;
+    pub mod websocket;
 }
 use crate::lib::fs::*;
 use crate::lib::rev_x::rev_message_in;
-
+use crate::lib::websocket::from_ws;
+use lib::websocket::RWebsocket;
 
 // structs
 mod structs {
@@ -34,109 +30,47 @@ mod structs {
     pub mod message;
     pub mod user;
 }
-use structs::auth::Auth;
-
 
 // external crates
-use futures_util::{StreamExt, SinkExt};
-use tokio_tungstenite::{connect_async, WebSocketStream};
-use std::str::from_utf8;
-use tokio::time::Duration;
-
-
-
-const PING: &str = r#"{
-    "type": "Ping",
-    "data": 0
-}"#;
-
+use futures_util::StreamExt;
 
 #[tokio::main]
-async fn main()  {
-
+async fn main() {
     println!("booting...");
-    
-    let details:Auth = conf_init()
-        .expect("Failed to import config/reywen.json");
 
-    let url:String = format!("wss://ws.revolt.chat/?format=json&version=1&token={}", details.token);    
-        
-    websocket(url, details).await; 
+    let auth = conf_init().expect("Failed to import config/reywen.json");
+    let ws_conf = ws_init().expect("failed to import ws from config/reywen.json");
 
-}
+    let websocket = RWebsocket::new(ws_conf);
 
-// establishes websocket connection
-pub async fn websocket(url: String, details: Auth) {
-
-
-     let (ws_stream, _response) = connect_async(url.clone()).await.expect("Failed to connect (websocket)");
-     let (ws_stream_ping, _response) = connect_async(url.clone()).await.expect("Failed to connect (websocket)");
-
-     println!("init: websocket");
-
-     
-     tokio::join! ( 
-         websocket_sub(ws_stream, details),
-         webocket_ping(ws_stream_ping),
-         );
-}
-
-pub async fn webocket_ping(mut ws_stream: WebSocketStream<tokio_tungstenite::MaybeTlsStream<tokio::net::TcpStream>>) {
 
     loop {
-        tokio::time::sleep(Duration::from_secs(30)).await;
-        let send_res = ws_stream.send(tokio_tungstenite::tungstenite::Message::Text(PING.to_string())).await;
-        
-        
-        send_res.expect("Failed to ping websocket, restarting");
-    };
+        println!("starting websocket");
+        websocket
+            .clone()
+            .generate()
+            .await
+            .for_each(|message| async {
+                let data = from_ws(message);
+                if data.is_empty() {
+                    println!("Caught ws error exiting websocket");
+                    return;
+                };
+                let input = rev_message_in(data);
 
+                let input_message = match input {
+                    Err(_) => return,
+                    Ok(a) => a,
+                };
+
+                tokio::join!(
+                    br_main(auth.clone(), &input_message),
+                    e6_main(auth.clone(), &input_message),
+                    message_main(auth.clone(), &input_message),
+                    plural_main(auth.clone(), &input_message),
+                    shell_main(auth.clone(), &input_message),
+                );
+            })
+            .await;
+    }
 }
-
-pub async fn websocket_sub(ws_stream: WebSocketStream<tokio_tungstenite::MaybeTlsStream<tokio::net::TcpStream>>, details: Auth) {
-    
-          
-    let (mut _write, read) = ws_stream.split();
-
-     
-    let read_future = read.for_each(|message| async {
-
-        let data = match message {
-             Ok(p) => {p.into_data()},
-             Err(e) => {panic!("{e}")},
-         };
-        
-        let out = from_utf8(&data).unwrap().to_string();
-
-       // moved websocket main to self contained function for ease of use
-
-        
-        new_main(out, details.clone()).await;
-     
-    });
-    read_future.await;
-  
-}
-
-// websocket main
-// imports messages, cleans them and sends to 
-// bridge and message processing
-pub async fn new_main(out: String, auth: Auth) {
-
-    let raw_message = rev_message_in(out);
-    
-    if raw_message.is_err() {return};
-    
-    let message = raw_message.expect("failed to process ws message");
-
-
-    tokio::join!(
-        br_main(auth.clone(), &message),
-        message_main(auth.clone(), &message),
-        shell_main(auth.clone(), &message),
-        plural_main(auth.clone(), &message),
-        e6_main(auth, &message),
-    );
-        
-}
-
