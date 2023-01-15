f89df321c2c548fd567862d4dc3114495473b8c9
ToastXC <100072983+toastxc@users.noreply.github.com>
2023-01-15 11:04:32 +0800

    removed primative method from lreywen (superseeded by oop)

--- src/lib/lreywen.rs
+++ src/lib/lreywen.rs
@@ -1,27 +1,8 @@
-// an abstraction layer for RevX2
-use crate::structs::message::{RMessagePayload, RMessage, RReplies};
-
-use super::rev_x::rev_send;
-
-pub async fn send(token: &str, message: &RMessage , content: &str) {
-
-    let reply = RReplies {
-        id: message._id.to_string(),
-        mention: false,
-    };
-    let payload2 = RMessagePayload {
-        content: Some(String::from(content)),
-        replies: Some(vec![reply]),
-          attachments: None,
-          masquerade: None
-    };
-
-    rev_send(token, &message.channel, payload2).await;
-}
+// an abstraction layer for RevX2 - functionality has been moved from here to oop
+use crate::structs::message::{RMessage, RReplies};
 
 #[allow(dead_code)]
 pub fn reply_from(input: &RMessage) -> RReplies {
-
     RReplies {
         id: input._id.to_owned(),
         mention: false,
@@ -32,16 +13,19 @@ pub fn lte(input: &str) -> String {
     format!("[]({input})")
 }
 
-
 // if the input message is not usable for reywen then return
 #[allow(dead_code)]
 pub fn crash_condition(input_message: &RMessage, character: Option<&str>) -> bool {
-
     if input_message.content.is_none() {
-        return true
+        return true;
     };
 
-    let temp_convec: Vec<&str> =  input_message.content.as_ref().unwrap().split(' ').collect::<Vec<&str>>();
+    let temp_convec: Vec<&str> = input_message
+        .content
+        .as_ref()
+        .unwrap()
+        .split(' ')
+        .collect::<Vec<&str>>();
 
     let mut length = 2;
 
@@ -50,17 +34,21 @@ pub fn crash_condition(input_message: &RMessage, character: Option<&str>) -> boo
     };
 
     if temp_convec.len() < length {
-        return true
+        return true;
     };
 
     if character.is_some() && character != Some(temp_convec[0]) {
-        return true
+        return true;
     };
-   false
+    false
 }
-
 
 #[allow(dead_code)]
 pub fn convec(input_message: &RMessage) -> Vec<&str> {
-    input_message.content.as_ref().unwrap().split(' ').collect::<Vec<&str>>()
+    input_message
+        .content
+        .as_ref()
+        .unwrap()
+        .split(' ')
+        .collect::<Vec<&str>>()
 }
