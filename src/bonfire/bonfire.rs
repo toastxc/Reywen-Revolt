d21db68301eed05e765e725a7e1d82e3f2bfdf61
ToastXC <100072983+toastxc@users.noreply.github.com>
2023-01-19 08:55:32 +0800

    cleaned up websocket and added pinging

--- src/bonfire/bonfire.rs
+++ src/bonfire/bonfire.rs
@@ -1,31 +1,48 @@
-use futures_util::StreamExt;
-use tokio_tungstenite::connect_async;
-
+// reywen
 use crate::quark::bonfire::RWebsocket;
-
+use futures_util::{stream::SplitSink, SinkExt, Stream, StreamExt};
+use tokio::net::TcpStream;
+use tokio_tungstenite::{
+    connect_async,
+    tungstenite::{error::Error, Message},
+    MaybeTlsStream, WebSocketStream,
+};
 impl RWebsocket {
     pub fn new(ws: RWebsocket) -> Self {
         ws
     }
 
-    pub async fn generate(
-        self,
-    ) -> futures_util::stream::SplitStream<
-        tokio_tungstenite::WebSocketStream<
-            tokio_tungstenite::MaybeTlsStream<tokio::net::TcpStream>,
-        >,
-    > {
+    pub async fn generate(self) -> impl Stream<Item = Result<Message, Error>> {
         let url = format!(
-            "wss://{}/?version=1format={}&&token={}",
+            "wss://{}/?version=1format={}&token={}",
             self.domain, self.format, self.token
         );
-        let ws = connect_async(url.clone())
+
+        let ws = connect_async(url)
             .await
             .expect("Failed to connect to websocket!");
         let (stream, _response) = ws;
 
-        let (_, read) = stream.split();
+        let (write, read) = stream.split();
+
+        tokio::spawn(ping_server(write));
+
         read
+    }
+}
+
+async fn ping_server(mut write: SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>) {
+    let wait = tokio::time::Duration::from_secs(30);
+
+    loop {
+        tokio::time::sleep(wait).await;
+
+        write
+            .send(Message::Text(String::from(
+                "{\n    \"type\": \"Ping\",\n    \"data\": 0\n}",
+            )))
+            .await
+            .unwrap();
     }
 }
 
