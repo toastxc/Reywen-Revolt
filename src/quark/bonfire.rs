1d2c9d3ddd9498b8f9f2f52c61a7cbbe7c1d0cc8
ToastXC <100072983+toastxc@users.noreply.github.com>
2023-01-29 10:53:27 +0800

    removed all old files

--- src/quark/bonfire.rs
+++
@@ -1,9 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Clone, Deserialize, Serialize, Default)]
-pub struct RWebsocket {
-    pub token: String,
-    pub format: String,
-    #[serde(rename = "websocket_domain")]
-    pub domain: String,
-}
