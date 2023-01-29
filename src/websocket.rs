21a6a392c1dd8ba4423e829bb804b798b01e4f4d
ToastXC <100072983+toastxc@users.noreply.github.com>
2023-01-29 10:55:36 +0800

    recreated websocket library

---
+++ src/websocket.rs
@@ -0,0 +1,70 @@
+// reywen
+use futures_util::{stream::SplitSink, SinkExt, Stream, StreamExt};
+use tokio::{net::TcpStream, sync::Mutex};
+use tokio_tungstenite::{
+    connect_async,
+    tungstenite::{error::Error, Message},
+    MaybeTlsStream, WebSocketStream,
+};
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Deserialize, Serialize, Default)]
+pub struct Websocket {
+    pub token: String,
+    pub format: String,
+    #[serde(rename = "websocket_domain")]
+    pub domain: String,
+}
+
+impl Websocket {
+    pub fn new(ws: Websocket) -> Self {
+        ws
+    }
+
+    pub async fn generate(self) -> impl Stream<Item = Result<Message, Error>> {
+        let url = format!(
+            "wss://{}/?version=1format={}&token={}",
+            self.domain, self.format, self.token
+        );
+
+        let ws = connect_async(url)
+            .await
+            .expect("Failed to connect to websocket!");
+        let (stream, _response) = ws;
+
+        let (write, read) = stream.split();
+        tokio::spawn(ping_server(Mutex::new(write)));
+
+        read
+    }
+}
+
+async fn ping_server(write: Mutex<SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>>) {
+    loop {
+        tokio::time::sleep(tokio::time::Duration::from_secs(20)).await;
+
+        write
+            .lock()
+            .await
+            .send(Message::Text(String::from(
+                "{\n    \"type\": \"Ping\",\n    \"data\": 0\n}",
+            )))
+            .await
+            .ok();
+    }
+}
+
+pub fn from_ws(
+    message: Result<tokio_tungstenite::tungstenite::Message, tokio_tungstenite::tungstenite::Error>,
+) -> String {
+    match message {
+        Ok(a) => a
+            .into_text()
+            .expect("websocket message failed (report to developer)"),
+        Err(e) => {
+            println!("Websocket Error!\n{}", e);
+            String::new()
+        }
+    }
+}
