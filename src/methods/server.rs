22dbbee76650357e3c768ea0d676cc82077952a0
ToastXC <100072983+toastxc@users.noreply.github.com>
2023-01-29 14:51:11 +0800

    created more methods for server

--- src/methods/server.rs
+++ src/methods/server.rs
@@ -1,3 +1,4 @@
+use iso8601_timestamp::Timestamp;
 use serde::{Deserialize, Serialize};
 use validator::Validate;
 
@@ -5,7 +6,8 @@ use crate::{
     debug::Web,
     structs::{
         channel::Channel,
-        server::{Category, FieldsServer, Server, SystemMessageChannels},
+        server::{Category, FieldsServer, Member, Server, SystemMessageChannels},
+        user::User,
     },
 };
 
@@ -72,6 +74,8 @@ pub struct DataEditServer {
     #[validate(length(min = 1))]
     remove: Option<Vec<FieldsServer>>,
 }
+// ################################## SERVER INFORMATION ##################################
+
 #[allow(dead_code)]
 pub async fn server_create(
     domain: &str,
@@ -105,7 +109,7 @@ pub async fn server_fetch(domain: &str, server: &str, token: &str) -> Option<Ser
         .await
     {
         Err(http_err) => {
-            Web::error(http_err, "message_fetch");
+            Web::error(http_err, "server_fetch");
             None
         }
         Ok(a) => match serde_json::from_str::<Server>(&a.text().await.unwrap()) {
@@ -127,7 +131,7 @@ pub async fn server_delete(
         .send()
         .await
     {
-        Web::error(e, "message_delete");
+        Web::error(e, "server_delete");
     };
 }
 #[allow(dead_code)]
@@ -140,7 +144,159 @@ pub async fn server_edit(domain: &str, server: &str, token: &str, server_edit: D
         .send()
         .await
     {
-        Web::error(e, "message_delete");
+        Web::error(e, "sever_delete");
     };
 }
 
+// ################################## SERVER MEMBERS ##################################
+#[derive(Serialize)]
+pub struct AllMemberResponse {
+    /// List of members
+    members: Vec<Member>,
+    /// List of users
+    users: Vec<User>,
+}
+/// Polls server for members - None for failure
+#[allow(dead_code)]
+pub async fn server_fetch_members(domain: &str, server: &str, token: &str) -> Option<Vec<Member>> {
+    match reqwest::Client::new()
+        .get(format!("https://{domain}/servers/{server}/members"))
+        .header("x-bot-token", token)
+        .send()
+        .await
+    {
+        Err(http_err) => {
+            Web::error(http_err, "server_fetch_members");
+            None
+        }
+        Ok(a) => match serde_json::from_str::<Vec<Member>>(&a.text().await.unwrap()) {
+            Err(_) => None,
+            Ok(a) => Some(a),
+        },
+    }
+}
+/// Polls server for a member - None for failure
+#[allow(dead_code)]
+pub async fn server_fetch_member(
+    domain: &str,
+    server: &str,
+    token: &str,
+    member: &str,
+) -> Option<Member> {
+    match reqwest::Client::new()
+        .get(format!(
+            "https://{domain}/servers/{server}/members/{member}"
+        ))
+        .header("x-bot-token", token)
+        .send()
+        .await
+    {
+        Err(http_err) => {
+            Web::error(http_err, "server_fetch_member");
+            None
+        }
+        Ok(a) => match serde_json::from_str::<Member>(&a.text().await.unwrap()) {
+            Err(_) => None,
+            Ok(a) => Some(a),
+        },
+    }
+}
+
+#[allow(dead_code)]
+pub async fn server_kick_member(domain: &str, server: &str, token: &str, member: &str) {
+    if let Err(e) = reqwest::Client::new()
+        .delete(format!(
+            "https://{domain}/server/{server}/members:/{member}"
+        ))
+        .header("x-bot-token", token)
+        .send()
+        .await
+    {
+        Web::error(e, "server_kick_member");
+    };
+}
+/// # Member Data
+#[derive(Validate, Serialize, Deserialize)]
+pub struct DataMemberEdit {
+    /// Member nickname
+    #[validate(length(min = 1, max = 32))]
+    nickname: Option<String>,
+    /// Attachment Id to set for avatar
+    avatar: Option<String>,
+    /// Array of role ids
+    roles: Option<Vec<String>>,
+    /// Timestamp this member is timed out until
+    timeout: Option<Timestamp>,
+    /// Fields to remove from channel object
+    #[validate(length(min = 1))]
+    remove: Option<Vec<FieldsMember>>,
+}
+/// Optional fields on server member object
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
+pub enum FieldsMember {
+    Nickname,
+    Avatar,
+    Roles,
+    Timeout,
+}
+
+#[allow(dead_code)]
+pub async fn server_edit_member(
+    domain: &str,
+    server: &str,
+    token: &str,
+    member: &str,
+    edit: DataMemberEdit,
+) {
+    if let Err(e) = reqwest::Client::new()
+        .patch(format!(
+            "https://{domain}/server/{server}/members:/{member}"
+        ))
+        .header("x-bot-token", token)
+        .header("Content-Type", "application/json")
+        .body(serde_json::to_string(&edit).unwrap())
+        .send()
+        .await
+    {
+        Web::error(e, "server_edit_member");
+    };
+}
+/// # Ban Information
+#[derive(Validate, Serialize, Deserialize)]
+pub struct DataBanCreate {
+    /// Ban reason
+    #[validate(length(min = 1, max = 1024))]
+    reason: Option<String>,
+}
+
+#[allow(dead_code)]
+pub async fn server_ban_member(
+    domain: &str,
+    server: &str,
+    token: &str,
+    member: &str,
+    reason: DataBanCreate,
+) {
+    if let Err(e) = reqwest::Client::new()
+        .patch(format!("https://{domain}/server/{server}/bans:/{member}"))
+        .header("x-bot-token", token)
+        .header("Content-Type", "application/json")
+        .body(serde_json::to_string(&reason).unwrap())
+        .send()
+        .await
+    {
+        Web::error(e, "server_ban_member");
+    };
+}
+
+#[allow(dead_code)]
+pub async fn server_unban_member(domain: &str, server: &str, token: &str, member: &str) {
+    if let Err(e) = reqwest::Client::new()
+        .delete(format!("https://{domain}/server/{server}/bans:/{member}"))
+        .header("x-bot-token", token)
+        .send()
+        .await
+    {
+        Web::error(e, "server_ban_member");
+    };
+}
--- src/structs/server.rs
+++ src/structs/server.rs
@@ -1,7 +1,10 @@
 use std::collections::HashMap;
 
+use iso8601_timestamp::Timestamp;
 use serde::{Deserialize, Serialize};
 use validator::Validate;
+
+use super::attachment::File;
 
 pub fn if_false(t: &bool) -> bool {
     !t
@@ -117,4 +120,37 @@ pub enum FieldsServer {
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 pub enum FieldsRole {
     Colour,
+}
+
+
+// ########################## SERVER MEMBERS ##########################
+#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+pub struct MemberCompositeKey {
+    /// Server Id
+    pub server: String,
+    /// User Id
+    pub user: String,
+}
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Member {
+    /// Unique member id
+    #[serde(rename = "_id")]
+    pub id: MemberCompositeKey,
+
+    /// Time at which this user joined the server
+    pub joined_at: Timestamp,
+
+    /// Member's nickname
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub nickname: Option<String>,
+    /// Avatar attachment
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub avatar: Option<File>,
+
+    /// Member's roles
+    #[serde(skip_serializing_if = "Vec::is_empty", default)]
+    pub roles: Vec<String>,
+    /// Timestamp this member is timed out until
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub timeout: Option<Timestamp>,
 }
