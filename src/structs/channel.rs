89ec377ee28b0cbd626a8db56e0c969ce6c2b1ec
ToastXC <100072983+toastxc@users.noreply.github.com>
2023-01-29 10:57:08 +0800

    created structures for channels

---
+++ src/structs/channel.rs
@@ -0,0 +1,101 @@
+use serde::{Deserialize, Serialize};
+
+pub fn if_false(t: &bool) -> bool {
+    !t
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(tag = "channel_type")]
+pub enum Channel {
+    /// Personal "Saved Notes" channel which allows users to save messages
+    SavedMessages {
+        /// Unique Id
+        #[serde(rename = "_id")]
+        id: String,
+        /// Id of the user this channel belongs to
+        user: String,
+    },
+    /// Direct message channel between two users
+    DirectMessage {
+        /// Unique Id
+        #[serde(rename = "_id")]
+        id: String,
+
+        /// Whether this direct message channel is currently open on both sides
+        active: bool,
+        /// 2-tuple of user ids participating in direct message
+        recipients: Vec<String>,
+        /// Id of the last message sent in this channel
+        #[serde(skip_serializing_if = "Option::is_none")]
+        last_message_id: Option<String>,
+    },
+    /// Group channel between 1 or more participants
+    Group {
+        /// Unique Id
+        #[serde(rename = "_id")]
+        id: String,
+
+        /// Display name of the channel
+        name: String,
+        /// User id of the owner of the group
+        owner: String,
+        /// Channel description
+        #[serde(skip_serializing_if = "Option::is_none")]
+        description: Option<String>,
+        /// Array of user ids participating in channel
+        recipients: Vec<String>,
+
+        /// Id of the last message sent in this channel
+        #[serde(skip_serializing_if = "Option::is_none")]
+        last_message_id: Option<String>,
+
+        /// Whether this channel is marked as not safe for work
+        #[serde(skip_serializing_if = "if_false", default)]
+        nsfw: bool,
+    },
+    /// Voice channel belonging to a server
+    VoiceChannel {
+        /// Unique Id
+        #[serde(rename = "_id")]
+        id: String,
+        /// Id of the server this channel belongs to
+        server: String,
+
+        /// Display name of the channel
+        name: String,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        /// Channel description
+        description: Option<String>,
+        /// Custom icon attachment
+
+        /// Whether this channel is marked as not safe for work
+        #[serde(skip_serializing_if = "if_false", default)]
+        nsfw: bool,
+    },
+}
+/// Partial values of [Channel]
+#[derive(Serialize, Deserialize, Debug, Default, Clone)]
+pub struct PartialChannel {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub name: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub owner: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub description: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub nsfw: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub active: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub permissions: Option<i64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub last_message_id: Option<String>,
+}
+
+/// Optional fields on channel object
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone)]
+pub enum FieldsChannel {
+    Description,
+    Icon,
+    DefaultPermissions,
+}
