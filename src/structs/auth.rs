89fa2f2323dc130ed6e693294c27615b3a451e68
ToastXC <100072983+toastxc@users.noreply.github.com>
2023-02-10 09:03:30 +0800

    added session type

--- src/structs/auth.rs
+++ src/structs/auth.rs
@@ -7,13 +7,21 @@ pub struct Auth {
     pub sudoers: Vec<String>,
     #[serde(rename = "api_domain")]
     pub domain: String,
+    pub header: String,
 }
 
 impl Auth {
-    pub fn from_token(token: &str) -> Self {
+    pub fn from_token(token: &str, is_bot: bool) -> Self {
+        let is_bot = match is_bot {
+            true => "x-bot-token",
+            false => "x-session-token",
+        }
+        .to_string();
+
         Auth {
             token: String::from(token),
             domain: (String::from("api.revolt.chat")),
+            header: is_bot,
             ..Default::default()
         }
     }
